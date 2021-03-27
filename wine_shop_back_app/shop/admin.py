292f839d887468a7d77b388e769b80f3051d8a34
Tornike <archvadzetornike@gmail.com>
2021-03-27 23:30:31 +0000

    fetch: added backend for ajax

--- wine_shop_back_app/shop/admin.py
+++ wine_shop_back_app/shop/admin.py
@@ -4,8 +4,18 @@ from django.contrib import admin
 
 from .models import *
 
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'image_tag']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'date_order', 'complete', 'transaction_id']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['product', 'order', 'quantity', 'date_added']
+
 admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
+
--- wine_shop_back_app/shop/views.py
+++ wine_shop_back_app/shop/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from shop.models import Product
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+import json
 
 # Create your views here.
+
+def wineproduct(request):
+    data = {'menu': ''}
+    if request.method == 'GET':
+        wine = request.GET.get('wine')
+        if wine:
+            data['menu'] = list(Product.objects.all().values())
+        else:
+            data['menu'] = wine + ' does not exists.'
+    return HttpResponse(json.dumps(data), content_type="application/json")
